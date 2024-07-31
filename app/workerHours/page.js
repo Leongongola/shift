9224f6f86517b12efb9af4d5f98819b7541cc044
leon gongola <leongongola@gmail.com>
2024-07-31 00:40:40 -0600

    defined suspense to

--- app/workerHours/page.js
+++ app/workerHours/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { getAuth } from "firebase/auth";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { useRouter, useSearchParams } from "next/navigation";
